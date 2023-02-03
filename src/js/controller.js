a89bd7d7c6d94e79d1ccb58ec6fb661b377cb937
Sammydepoj <samueladepoju86@yahoo.com>
2023-02-03 17:58:27 +0100

    first commit

---
+++ src/js/controller.js
@@ -0,0 +1,66 @@
+import * as model from './model.js';
+import recipeView from './views/recipeView.js';
+import searchView from './views/searchView.js';
+import resultsView from './views/resultsView.js';
+import PaginationView from './views/paginationView.js';
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+
+// if(module.hot){
+//   module.hot.accept();
+// }
+
+const controlRecipes = async function () {
+  try {
+    const id = window.location.hash.slice(1);
+    if (!id) return;
+
+    recipeView.renderSpinner();
+
+    // 1. Loading recipe
+    await model.loadRecipe(id);
+
+    // 2. Rendering recipe
+    recipeView.render(model.state.recipe);
+
+  } catch (err) {
+    recipeView.renderError();
+  }
+}
+
+const controlSearchResults = async function () {
+  try {
+    resultsView.renderSpinner();
+
+    // 1. Get search query
+    const query = searchView.getQuery();
+    if (!query) return;
+
+    // 2. Load Search query
+    await model.loadSearchResults(query);
+
+    // 3. Render Results
+    // console.log(model.state.search.results);
+    resultsView.render(model.getSearchResultsPage());
+
+    //4. render initial pagination buttons
+    PaginationView.render(model.state.search);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const controlPagination = function (goToPage) {
+  // 1) Render NEW results
+  resultsView.render(model.getSearchResultsPage(goToPage));
+
+  // 2) Render NEW pagination buttons
+  PaginationView.render(model.state.search);
+};
+
+const init = function () {
+  recipeView.addHandlerRender(controlRecipes);
+  searchView.addHandlerSearch(controlSearchResults);
+  PaginationView.addHandlerClick(controlPagination);
+}
+init()
